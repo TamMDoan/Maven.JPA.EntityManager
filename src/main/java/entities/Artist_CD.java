7eeaa351c70a198e5200f546b67c1628e492c1b7
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-12-21 21:09:13 -0500

    created artist_cd

--- src/main/java/entities/Artist_CD.java
+++ src/main/java/entities/Artist_CD.java
@@ -1,2 +1,27 @@
-package entities;public class Artist_CD {
+package entities;
+
+public class Artist_CD {
+    int cd_id;
+    int artist_id;
+
+    public Artist_CD(int cd_id, int artist_id){
+        this.cd_id = cd_id;
+        this.artist_id = artist_id;
+    }
+
+    public int getCd_id() {
+        return cd_id;
+    }
+
+    public void setCd_id(int cd_id) {
+        this.cd_id = cd_id;
+    }
+
+    public int getArtist_id() {
+        return artist_id;
+    }
+
+    public void setArtist_id(int artist_id) {
+        this.artist_id = artist_id;
+    }
 }

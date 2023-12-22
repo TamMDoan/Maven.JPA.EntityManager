ed664a0727d1daf5746a5d44ad7e784fa3a6dc98
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-12-21 21:08:47 -0500

    created cd

--- src/main/java/entities/CD.java
+++ src/main/java/entities/CD.java
@@ -1,2 +1,69 @@
-package entities;public class CD {
+package entities;
+
+
+public class CD {
+
+    int id;
+    String title;
+    String description;
+    int year;
+    String artist;
+    double price;
+
+    public CD(int id, String title, String description, int year, String artist, double price){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.year = year;
+        this.artist = artist;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
